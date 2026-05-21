a69db8da3affd8ef123f438935e14d5d72f4d694
Alícia Stefany <64162281+aliciastefany@users.noreply.github.com>
2026-05-21 20:27:49 +0000

    update form

---
+++ frontend/src/App.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+function App(){
+  const [alunos, setAlunos] = useState([])
+
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [curso, setCurso] = useState("")
+
+  const [editando, setEditando] = useState(false)
+  const [idAtual, setIdAtual] = useState(null)
+
+  async function salvar(e){
+    e.preventDefault()
+    const aluno = [nome, email, curso]
+
+    if(editando){
+      await axios.put(`https://cuddly-fortnight-9w94wv4r4g6f95xp-3001.app.github.dev/alunos/${idAtual}`,aluno)
+      setEditando(false)
+      setIdAtual(null)
+    } else {
+      await axios.post("https://cuddly-fortnight-9w94wv4r4g6f95xp-3001.app.github.dev/alunos", aluno)
+      //limparFormulario()
+      //buscarAlunos()
+    }
+  }
+
+  return(
+    <div style={{padding:20}}>
+      <h1>CRUD de Alunos</h1>
+
+      <form onSubmit={salvar}>
+        <input
+          type="text"
+          placeholder="Nome"
+          value={nome}
+          onChange={(texto)=>setNome(texto.target.value)}
+        />
+        <br/>
+        <input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(texto)=>setEmail(texto.target.value)}
+        />
+        <br/>
+        <input
+          type="text"
+          placeholder="Curso"
+          value={curso}
+          onChange={(texto)=>setCurso(texto.target.value)}
+        />
+        <br/>
+        <button type="submit">
+          {editando ? "Atualizar" : "Cadastrar"}
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default App
